66e100ee8c3a4b5be293dcce2ee8f598b51b05da
DOstapovets <dimaost97@gmail.com>
2017-02-08 01:25:30 +0200

    Revert "Revert "Structerd""
    This reverts commit 3f6f12221ee83d751557815d7abb05cfca394cba.

--- script.py
+++ script.py
@@ -59,7 +59,6 @@ class Parser:
 output_file = open("out.csv", "wb")
 wr = csv.writer(output_file)
 
-<<<<<<< HEAD
 # parameters for site oktv
 # selector must be in XPath
 req = Params("http://oktv.ua/search", selectors={
@@ -77,33 +76,3 @@ Oktv = Parser(req)
 oktv_flats = Oktv.getAllFlats()
 for flat in oktv_flats:
     wr.writerow([flat.url.encode('raw-unicode-escape'), flat.img.encode('raw-unicode-escape'), flat.address.encode('raw-unicode-escape'),flat.price.encode('raw-unicode-escape')])
-=======
-page = 0
-date_start = datetime.date(2017, 02, 07)
-date_finish = (date_start + datetime.timedelta(days=1))
-while True:
-    values = {'order_start': date_start.strftime("%d.%m.%Y"),
-              'order_finish': date_finish.strftime("%d.%m.%Y"),
-              'start': page}
-    data = urllib.urlencode(values)
-    req = urllib2.Request(url=url, data=data, headers=headers)
-    response = urllib2.urlopen(req)
-
-    html = response.read()
-    soup = BeautifulSoup(html)
-    apart_list = soup.find('div', {'class': 'ajax-pagination-content'}).findAll('div', {'class': 'object_v_spiske'})
-    if not len(apart_list):
-        break
-    for item in apart_list:
-        apart = []
-        apart.append(url+item.find('a').get('href'));
-        apart.append(url+item.find('img').get('src'))
-        apart.append(''.join(item.find('div', {'class': 'object_price'}).findAll(text=True)).strip())
-        apart.append(''.join(item.find('div', {'class': 'object_title'}).findAll(text=True)).strip())
-        ul = item.find('ul').findAll('li')
-        for li in ul:
-            apart.append(''.join(li.findAll(text=True)).strip())
-        wr.writerow([unicode(s).encode("utf-8") for s in apart])
-    page += 12
-output_file.close()
->>>>>>> parent of f5fb163... Structerd
